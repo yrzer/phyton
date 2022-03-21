86c9377426623fafc8c54416f2af351369fd70f3
yrzer <89992153+yrzer@users.noreply.github.com>
2022-03-21 21:24:55 +0100

    r

--- maine/index.py
+++ maine/index.py
@@ -1,37 +1,120 @@
-from textwrap import fill
-from turtle import color
 import pygame
 import sys
-import copy
+import os
 import random
-import time
 
 pygame.init()
 
-wys = pygame.display.set_mode((800,600))
-pygame.display.update()
-box = pygame.Rect(10,50,100,100) # pozycja początkowa a,b wystokość i szerokość c,d
-color = [(0,255,0), (255,0,0), (0,0,255)]
-nr_c = 0
+win = pygame.display.set_mode((1920/3,1080/3)) # 1920 × 1080
+pygame.display.set_caption("dino adventures")
+
+path = "maine\dino\\"
+dino = pygame.image.load(path+'dino-stand.png')
+restart = pygame.image.load(path+'restart.png')
+game_over = pygame.image.load(path+'game_over.png')
+chmura = pygame.image.load(path+'chmura.png')
+backline = pygame.image.load(path+'backline.png')
+dino_r = []
+dino_r.append(pygame.image.load(path+'dino-right.png'))
+dino_r.append(pygame.image.load(path+'dino-left.png'))
+ptak = []
+ptak.append(pygame.image.load(path+'ptak1.png'))
+ptak.append(pygame.image.load(path+'ptak2.png'))
+liczby= []
+for x in range(10):
+    liczby.append(pygame.image.load(path+str(x)+'.png'))
+liczby.append(pygame.image.load(path+'H.png'))
+liczby.append(pygame.image.load(path+'I.png'))
+kaktus = pygame.image.load(path+'kaktus1.png')
+
+clock = pygame.time.Clock()
+delta = 0
+sekunda = 0
+do_10 = 0
+do_100 = 0
+backlineX = 5
+w_dino_r = 0
+spacja = False
+delta_v2 =0
+dino_rY = 275
+G = [1,1,2,2,2,3,3,3,3,4,4,4,4,4,5,5,5,5,6,6,6,6,6,6,7,7,7,8,8,8,9]
+G.reverse()
+kaktusX = 700
+
 while True:
+    #czas
+    clock.tick(60) # jeszcze zrobie lepsze ?
+    delta += 1
+    if(delta == 60):
+        delta = 1
+        sekunda +=1
+        do_10+=1
+        if(do_10==10):
+            do_10=0
+            do_100+=1
+            if(do_100==10):
+                do_100=0
+    #czas
+    
     for event in pygame.event.get():
         # wyjście
         if event.type == pygame.QUIT: 
             sys.exit(0)
-        # wyjście
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_r:
-            nr_c = 1
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_g:
-            nr_c = 0
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_b:
-            nr_c = 2
-    mouse_x, mouse_y = pygame.mouse.get_pos() 
-    box.x = mouse_x - 50
-    box.y = mouse_y - 50
-       
-    wys.fill((0,0,0))
-    pygame.draw.rect(wys, color[nr_c], box)
+        # skakanie
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            spacja= True
+    
+    #skakanie
+    if spacja==True :
+        delta_v2 += 1
+        if(delta_v2 == 30):
+            G.reverse()
+        if(delta_v2 <30):
+            dino_rY-=G[delta_v2]
+        else:
+            dino_rY+=G[delta_v2-29]
+        if(delta_v2==58):
+            G.reverse()
+            spacja = False
+            delta_v2=0
+        
+        
+    
+    win.fill((0,0,0))
+    #rysowanie
+    
+    #licznik
+    win.blit(liczby[do_10], (610, 10))
+    win.blit(liczby[do_100], (600, 10))
+    
+    #tło
+    backlineX -= 5
+    if(backlineX<-1200): backlineX=0
+    if(backlineX<-560):  win.blit(backline, (backlineX+1200, 315))
+    win.blit(backline, (backlineX, 315))
+    
+    #random kaktus
+    kaktusX -= 5
+    if(kaktusX <-10):
+        kaktusX = 700
+    win.blit(kaktus, (kaktusX, 272)) # 25x50
+
+    #dino
+    if(delta%6==0):
+        w_dino_r +=1
+        if(w_dino_r==2):
+            w_dino_r=0
+    win.blit(dino_r[w_dino_r], (30, dino_rY)) # 44 x 46
+    
+    #kolsizja kaktus i dino
+    if (kaktusX > 28 and kaktusX < 52) and (dino_rY < 277 and dino_rY > 238):
+       pass# print("kolizja")  ekran końcowy
+    
+    #print(kaktusX, dino_rY)
+    
+    
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    #print(mouse_x, mouse_y)
+    
+    pygame.display.update()
     pygame.display.flip()
-    
-    
-#
--- maine/trial.py
+++ maine/trial.py
@@ -1,24 +1,35 @@
-# Taken from husano896's PR thread (slightly modified)
 import pygame
-from pygame.locals import *
+import sys
+import copy
+import random
+import time
 pygame.init()
-screen = pygame.display.set_mode((640, 480))
+wys = pygame.display.set_mode((800,600))
+pygame.display.update()
+box = pygame.Rect(10,50,100,100) # pozycja początkowa a,b wystokość i szerokość c,d
+color = [(0,255,0), (255,0,0), (0,0,255)]
+nr_c = 0
 clock = pygame.time.Clock()
-
-def main():
-   while True:
-      for event in pygame.event.get():
-            if event.type == QUIT:
-               pygame.quit()
-               return
-            elif event.type == MOUSEWHEEL:
-               print(event)
-               print(event.x, event.y)
-               print(event.flipped)
-               print(event.which)
-               # can access properties with
-               # proper notation(ex: event.y)
-      clock.tick(60)
-
-# Execute game:
-main()
+while True:
+    for event in pygame.event.get():
+        # wyjście
+        if event.type == pygame.QUIT: 
+            sys.exit(0)
+        # wyjście
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+            nr_c = 1
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_g:
+            nr_c = 0
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_b:
+            nr_c = 2
+    
+    dt = clock.tick()
+    
+    mouse_x, mouse_y = pygame.mouse.get_pos() 
+    box.x = mouse_x - 50
+    box.y = mouse_y - 50
+ #   keys= pygame.key.get_pressed()
+ #   if keys[pygame.K_w]:
+    wys.fill((0,0,0))
+    pygame.draw.rect(wys, color[nr_c], box)
+    pygame.display.flip()
